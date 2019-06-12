6e263172cd4abb56ea980f3591a6f4a93df9e1fb
Arrow <46056460+tanjowen@users.noreply.github.com>
2019-06-11 18:34:10 -0700

    started js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,25 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+	top: 20;
+	bottom: 40;
+	right: 40;
+	left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3
+	.select("#scatter")
+	.append("svg")
+	.attr("width", svgWidth)
+	.attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+	.attr("transform", `translate(${margin.left}, ${margin.top}`);
+
+var chosenXAxis = "poverty";
+
+function xScale()
